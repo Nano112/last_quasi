c9a059c6da4fbb94ff514d57d5ff07f7f29dc734
Nano112 <harrisonbastian.nano@gmail.com>
2021-07-16 17:25:38 +0100

    fixed page loading

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template
 from datetime import datetime
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 time = datetime.now()
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    global time
+    return render_template('index.html', time=datetime.timestamp(time))
 
 
 @app.route('/api/get-time', methods=['GET'])
 def get_time():
-    print(time)
+    global time
     return time.strftime('%s')
+
 
 @app.route('/api/reset-time', methods=['GET'])
 def reset_time():
     global time
     time = datetime.now()
-    print(time)
     return ''
